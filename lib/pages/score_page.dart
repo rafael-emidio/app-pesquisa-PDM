6804d6e06f8ae21bcea7faf96a165a092efa4b99
JhenyferFG <76816069+JhenyferFG@users.noreply.github.com>
2021-05-15 14:14:43 -0300

    Update score_page.dart

--- lib/pages/score_page.dart
+++ lib/pages/score_page.dart
@@ -1,7 +1,9 @@
 import 'package:app_pesquisa_pdm/services/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:random_string/random_string.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
+import '../banco/restaurantes_rep.dart';
 import '../controllers/restaurante_controller.dart';
 import '../models/restauranteModel.dart';
 
@@ -27,119 +29,122 @@ class _ScorePageState extends State<ScorePage> {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-        appBar: AppBar(
-          title: Text("Pesquisa de satisfação"), //widget.user.nome
-          bottom: TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.stacked_bar_chart),
-                text: 'Pontos',
-              ),
-              Tab(
-                icon: Icon(Icons.star_outlined),
-                text: 'Descontos',
-              ),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: [
-            Column(
-              //mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(0),
-                  child: Image.asset('assets/images/score.png'),
+          appBar: AppBar(
+            title: Text("Pesquisa de satisfação"), //widget.user.nome
+            bottom: TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.stacked_bar_chart),
+                  text: 'Pontos',
                 ),
-                Text(
-                  'Sua pontuação atual é 0', //${widget.user.score}
-                  style: TextStyle(fontSize: 24),
+                Tab(
+                  icon: Icon(Icons.star_outlined),
+                  text: 'Descontos',
                 ),
               ],
             ),
-            Container(
-              child: ListView.separated(
-                itemCount: controller.restaurantes.length,
-                separatorBuilder: (_, __) => Divider(),
-                itemBuilder: (BuildContext context, int i) {
-                  final List<RestauranteModel> lista =
-                      controller.restaurantes; // lista de restaurantes
-                  return ListTile(
-                    leading: Image.network(
-                      lista[i].url,
-                      width: 65,
+          ),
+          body:
+              Consumer<RestaurantesRep>(builder: (context, repositorio, child) {
+            return TabBarView(
+              children: [
+                Column(
+                  //mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(0),
+                      child: Image.asset('assets/images/score.png'),
                     ),
-                    title: Text(lista[i].nome),
-                    trailing: Text(
-                      '5%',
-                      style: TextStyle(
-                        color: Colors.green,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
+                    Text(
+                      'Sua pontuação atual é 0', //${widget.user.score}
+                      style: TextStyle(fontSize: 24),
                     ),
-                    onTap: () {
-                      Alert(
-                        context: context,
-                        //type: AlertType.info,
-                        image: Image.asset(
-                          "assets/images/cupom.png",
-                          width: 170,
+                  ],
+                ),
+                Container(
+                  child: ListView.separated(
+                    itemCount: repositorio.restaurantes.length,
+                    separatorBuilder: (_, __) => Divider(),
+                    itemBuilder: (BuildContext context, int i) {
+                      final List<RestauranteModel> lista =
+                          repositorio.restaurantes; // lista de restaurantes
+                      return ListTile(
+                        leading: Image.network(
+                          lista[i].url,
+                          width: 65,
                         ),
-                        title: "Resgate de Cupom",
-                        desc:
-                            "Deseja resgatar um cupom de desconto para este estabelecimento ?",
-                        buttons: [
-                          DialogButton(
-                            child: Text(
-                              "RETORNAR",
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 18),
-                            ),
-                            onPressed: () => Navigator.pop(context),
-                            width: 120,
+                        title: Text(lista[i].nome),
+                        trailing: Text(
+                          '5%',
+                          style: TextStyle(
+                            color: Colors.green,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
                           ),
-                          DialogButton(
-                            child: Text(
-                              "RESGATAR",
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                        onTap: () {
+                          Alert(
+                            context: context,
+                            //type: AlertType.info,
+                            image: Image.asset(
+                              "assets/images/cupom.png",
+                              width: 170,
                             ),
-                            onPressed: () {
-                              Alert(
-                                context: context,
-                                title: "CUPOM",
-                                content: Column(
-                                  children: <Widget>[
-                                    Text(randomNumeric(6)),
-                                  ],
+                            title: "Resgate de Cupom",
+                            desc:
+                                "Deseja resgatar um cupom de desconto para este estabelecimento ?",
+                            buttons: [
+                              DialogButton(
+                                child: Text(
+                                  "RETORNAR",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 18),
                                 ),
-                                buttons: [
-                                  DialogButton(
-                                    child: Text(
-                                      "RETORNAR",
-                                      style: TextStyle(
-                                          color: Colors.white, fontSize: 20),
+                                onPressed: () => Navigator.pop(context),
+                                width: 120,
+                              ),
+                              DialogButton(
+                                child: Text(
+                                  "RESGATAR",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 18),
+                                ),
+                                onPressed: () {
+                                  Alert(
+                                    context: context,
+                                    title: "CUPOM",
+                                    content: Column(
+                                      children: <Widget>[
+                                        Text(randomNumeric(6)),
+                                      ],
                                     ),
-                                    onPressed: () => Navigator.pop(context),
-                                    width: 120,
-                                  )
-                                ],
-                              ).show();
-                            },
-                            width: 120,
-                          )
-                        ],
-                      ).show();
+                                    buttons: [
+                                      DialogButton(
+                                        child: Text(
+                                          "RETORNAR",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 20),
+                                        ),
+                                        onPressed: () => Navigator.pop(context),
+                                        width: 120,
+                                      )
+                                    ],
+                                  ).show();
+                                },
+                                width: 120,
+                              )
+                            ],
+                          ).show();
+                        },
+                      );
                     },
-                  );
-                },
-                padding: EdgeInsets.all(16),
-              ),
-            )
-          ],
-        ),
-      ),
+                    padding: EdgeInsets.all(16),
+                  ),
+                )
+              ],
+            );
+          })),
     );
   }
 }
