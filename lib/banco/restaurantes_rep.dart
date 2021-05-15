9d8fae859345ebc233be6fb129ab357570b77d64
JhenyferFG <76816069+JhenyferFG@users.noreply.github.com>
2021-05-15 14:08:25 -0300

    Update restaurantes_rep.dart

--- lib/banco/restaurantes_rep.dart
+++ lib/banco/restaurantes_rep.dart
@@ -41,4 +41,47 @@ class RestaurantesRep {
     //]);
     this.getRestaurantes();
   }
+}import 'dart:collection';
+
+import 'package:flutter/material.dart';
+
+import '../models/restauranteModel.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'db_firestore.dart';
+
+class RestaurantesRep extends ChangeNotifier {
+  List<RestauranteModel> _restaurantes = [];
+  UnmodifiableListView<RestauranteModel> get restaurantes =>
+      UnmodifiableListView(_restaurantes);
+
+  getRestaurantes() async {
+    FirebaseFirestore db = await DBFirestore.get();
+    var snapshot = await db.collection('restaurantes').get();
+
+    snapshot.docs.forEach((doc) {
+      final data = doc.data();
+      _restaurantes.add(RestauranteModel(
+        id: doc.id,
+        email: data['email'],
+        nome: data['nome'],
+        proprietario: data['proprietario'],
+        url: data['url'],
+      ));
+    });
+    notifyListeners();
+    return _restaurantes;
+  }
+
+  RestaurantesRep() {
+    //_restaurantes.addAll([
+    //  RestauranteModel(
+    //    id: '1',
+    //    email: 'Restaurante elite',
+    //    nome: 'teste',
+    //    proprietario: 'prop',
+    //   url: 'https://restauranteelite.com.br/wp-content/uploads/2016/05/logoeliteblue.png',
+    // ),
+    //]);
+    this.getRestaurantes();
+  }
 }
