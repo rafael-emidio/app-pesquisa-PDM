974e75f9f195aa58e725ec36441b7a6d1ff62f08
JhenyferFG <76816069+JhenyferFG@users.noreply.github.com>
2021-05-15 14:12:18 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
+import 'package:provider/provider.dart';
 import './services/auth_service.dart';
 import './pages/pagina_inicial.dart';
 import './pages/login.dart';
 import './pages/checkauth.dart';
+import 'banco/restaurantes_rep.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   Get.lazyPut<AuthService>(() => AuthService());
 
-  runApp(AppPesquisa());
+  runApp(ChangeNotifierProvider(
+      create: (context) => RestaurantesRep(), child: AppPesquisa()));
 }
 
 class AppPesquisa extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp(
       title: 'Pesquisas de Satisfação',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
